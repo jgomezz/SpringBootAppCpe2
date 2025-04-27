52d79bb76a03ff319b0562f0bb2d4ff44fe32709
jgomezz <jgomezz@gmail.com>
2025-04-27 09:07:36 -0500

    first commit

---
+++ src/main/java/pe/edu/tecsup/springnootapp/SpringBootAppCpeApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.springnootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppCpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppCpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/tecsup/springnootapp/SpringBootAppCpeApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.springnootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppCpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
